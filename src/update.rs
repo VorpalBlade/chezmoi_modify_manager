4da2b9bd8801227954e3e14f1e45ac07c45c588a
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2023-12-28 09:57:05 +0100

    feat: Make automated updates possible (--no-confirm flag)

--- src/update.rs
+++ src/update.rs
@@ -4,13 +4,14 @@
 use self_update::cargo_crate_version;
 
 /// Perform update check from github.
-pub(super) fn update() -> anyhow::Result<()> {
+pub(super) fn update(no_confirm: bool) -> anyhow::Result<()> {
     let status = self_update::backends::github::Update::configure()
         .repo_owner("VorpalBlade")
         .repo_name("chezmoi_modify_manager")
         .bin_name("chezmoi_modify_manager")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
+        .no_confirm(no_confirm)
         .build()?
         .update()?;
     println!("Update status: `{}`!", status.version());
